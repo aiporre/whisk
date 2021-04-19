fe5f72db5ef5ff30bd2c0632c43106c3a35d8c40
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2021-04-19 16:29:08 +0200

    created setup module config

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='whisk',
+   version='0.1.0',
+   author='',
+   author_email='',
+   packages=['whisk'],
+   # scripts=['bin/script1','bin/script2'],
+   url='http://pypi.python.org/pypi/whisk/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.markdown').read(),
+   install_requires=[],
+)
