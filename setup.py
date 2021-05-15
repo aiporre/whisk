97aab742648ea49bf3b9583442aa7c6b441ba97b
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2021-05-16 01:08:42 +0200

    include binaries in the package

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
    name='whisk',
    version='0.1.0',
-   author='',
+   author='Janelia labs.',
    author_email='',
    packages=['whisk'],
-   # scripts=['bin/script1','bin/script2'],
+   # scripts=['bin/trace.exe','bin/traj', 'bin/classify', 'bin/classify-single-whisker', 'bin/classify_radial', 'bin/measure'],
+   # scripts=['bin/trace.exe'],
    url='http://pypi.python.org/pypi/whisk/',
    license='LICENSE.txt',
-   description='An awesome package that does something',
+   description='whisk janelia: whisker tracking system',
    long_description=open('README.markdown').read(),
    install_requires=[],
+   include_package_data=True,
 )
